5b5ee780b6af376540049f67ad5cce2ff25f146a
divakar0210 <reachtechnologiesservices@gmail.com>
2024-08-31 09:23:58 +0530

    Abdul Pushing All Update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Social from "@/components/Social";
 import Stats from "@/components/Stats";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
+import Link from "next/link";
 
 const page = () => {
   return (
@@ -20,22 +21,31 @@ const page = () => {
               Proficient in vatious Programming Language and technoligies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-                <span>Download CV</span>
-                <FiDownload />
-              </Button>
+              <a href="AbdulRahamanJagirdharResume.pdf" download>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0 ">
-                <Social containerStyles="flex gap-6" iconStyles="size-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent  hover:text-primary hover:transition-all duration-500"/>
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="size-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent  hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
-          </div >
+          </div>
           {/* //Photo */}
           <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo/>
+            <Photo />
           </div>
         </div>
       </div>
-      <Stats/>
+      <Stats />
     </section>
   );
 };
